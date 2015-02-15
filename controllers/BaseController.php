872dad6405b97c18c7c7d7606dbf8df4bf288e77
Ary Wibowo <nucreativa@gmail.com>
2015-02-15 09:18:49 +0700

    Clean unused view files

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -8,5 +8,13 @@ use yii\web\Controller;
 class BaseController extends Controller
 {
     
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
     
 }
